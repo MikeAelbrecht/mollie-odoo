aecd74044ba4c15a29c948f770fc9039bb1b3f7d
Parth Gajjar <parthegajjar@gmail.com>
2024-06-18 14:13:01 +0530

    [IMP] added POS kiosk module and improvements

--- mollie_pos_terminal/controllers/main.py
+++ mollie_pos_terminal/controllers/main.py
@@ -8,9 +8,14 @@ _logger = logging.getLogger(__name__)
 
 class PosMollieController(http.Controller):
 
-    @http.route('/pos_mollie/webhook', type='http', methods=['POST'], auth='public', csrf=False)
-    def webhook(self, **post):
+    # TODO: '/pos_mollie/webhook' is for backwards compatibility
+    # Remove it in v18
+    @http.route([
+        '/pos_mollie/webhook',
+        '/pos_mollie/webhook/<string:order_type>',
+    ], type='http', methods=['POST'], auth='public', csrf=False)
+    def webhook(self, order_type='pos', **post):
         if not post.get('id'):
             return
-        request.env['mollie.pos.terminal.payments']._mollie_process_webhook(post)
+        request.env['mollie.pos.terminal.payments']._mollie_process_webhook(post, order_type)
         return ""
---
+++ mollie_pos_terminal_self/models/pos_config.py
@@ -0,0 +1,4 @@
+from odoo.addons.pos_self_order.models.pos_config import PosConfig
+
+
+PosConfig._ALLOWED_PAYMENT_METHODS.append('mollie')
